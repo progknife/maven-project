6409780372190355964569a78ef4d09389d92b3f
Karl Manuzon <kmanuzon@deloitte.com>
2018-05-18 16:40:00 -0400

    Fix javadoc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-   * @param string someone the name of a person
-   * @return string greeting string
+   * @param someone the name of a person
+   * @return greeting string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
